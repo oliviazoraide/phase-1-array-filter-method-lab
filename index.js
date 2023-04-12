482462d5f64ff5ce289b6646a83d8e824b2b1192
Olivia Desgrossillier <oliviadesgrossillier@gmail.com>
2023-04-12 00:09:36 -0700

    completed filter lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+
+function findMatching(drivers, string) {
+    return drivers.filter(element => element.toLowerCase() === string.toLowerCase() ? string : undefined)
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(element => element.substring(0, 2) === string.substring(0,2) ? string : undefined)
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(obj => obj.name === string ? obj : undefined)
+}
